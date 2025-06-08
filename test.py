cabed9f81998b9f3f0849fd9e3b856d49b828ad0
krisvih32 <krisvih32@yahoo.com>
2025-06-08 10:05:25 -0700

    Removed faulty debugger

--- test.py
+++
@@ -1,6 +0,0 @@
-from pybricks.pupdevices import Motor
-from pybricks.parameters import Port, Direction
-hold = Motor(Port.A, Direction.CLOCKWISE, reset_angle=False, profile=5)
-spin = Motor(Port.C, Direction.CLOCKWISE, reset_angle=False, profile=5)
-move = Motor(Port.E, Direction.CLOCKWISE, reset_angle=False, profile=5)
-print("Hold: ", hold.angle(), "Spin: ", spin.angle(), "Move: ", move.angle())
